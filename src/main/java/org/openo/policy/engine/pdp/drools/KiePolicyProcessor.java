ea6ea64174b64d5345fcd2aba6f4d7fc53381638
wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>
2017-02-17 10:54:53 +0800

    Add Drools Dependeny in pom
    Change-Id: I45b081e7e1919a8528003763a6f36ccb5a18a513
    Issue-ID:POLICY-49
    Signed-off-by: wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>

---
+++ src/main/java/org/openo/policy/engine/pdp/drools/KiePolicyProcessor.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openo.policy.engine.pdp.drools;
+
+import org.openo.policy.engine.pdp.PolicyProcessor;
+import org.openo.policy.engine.pep.PolicyAction;
+
+public class KiePolicyProcessor implements PolicyProcessor{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Object fact) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int fire() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reload() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPolicyAction(PolicyAction action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unregisterChannel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
