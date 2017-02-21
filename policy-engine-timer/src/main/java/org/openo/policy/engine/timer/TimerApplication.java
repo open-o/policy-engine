e7f464ba64f1fd2ec0163311b251a28b2fc4d985
liyibo <li.yibo@zte.com.cn>
2017-02-21 16:24:51 +0800

    Realize timer policy
    add dropwizard Application and Configuration for Timer
    
    Change-Id: Idca1eb23414f0fa69901ba465a047f2a7ea0bdec
    Issue-ID: POLICY-52
    Signed-off-by: liyibo <li.yibo@zte.com.cn>

---
+++ policy-engine-timer/src/main/java/org/openo/policy/engine/timer/TimerApplication.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.policy.engine.timer;
+
+import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TimerApplication extends Application<TimerConfiguration> {
+    @Override
+    public void run(TimerConfiguration configuration, Environment environment) throws Exception {
+
+    }
+
+    @Override
+    public void initialize(Bootstrap<TimerConfiguration> bootstrap) {
+        super.initialize(bootstrap);
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+        new TimerApplication().run(args);
+    }
+}
